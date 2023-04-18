b63d4c6509ab73a268f8752bd67df406f9d6de70
Federico Zarate <federicozarate218@gmail.com>
2023-04-18 19:40:21 -0300

    ciclos y condicionales

---
+++ app.js
@@ -0,0 +1,23 @@
+// ejemplo de condicional
+
+let edad = 20;
+if (edad <= 17) {
+  console.log("eres mayor");
+} else {
+  console.log("eres menor de edad");
+}
+
+if (edad >= 18) {
+  console.log("eres mayor de edad");
+} else {
+  console.log("eres menor de edad");
+}
+
+// ejemplo de ciclo
+let contador = 0;
+
+while (contador < 10) {
+  console.log(contador);
+  contador++;
+  0;
+}
